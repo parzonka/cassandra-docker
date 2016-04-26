f9407dd76c6ab4c1ba621730e304edc14466e3df
Mateusz Parzonka <parzonka@gmail.com>
2016-04-26 20:59:48 +0200

    Let seeds and cluster name be configurable

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,14 @@
 sed -i -e "s/^rpc_address:.*/rpc_address: 0.0.0.0/" /etc/cassandra/cassandra.yaml
 sed -i -e "s/^# broadcast_rpc_address:.*/broadcast_rpc_address: localhost/" /etc/cassandra/cassandra.yaml
 
+if [ -z "$SEEDS" ]; then echo "No seeds set" ; else 
+	sed -i -e "s/- seeds: \"127.0.0.1\"/- seeds: \"$SEEDS\"/" /etc/cassandra/cassandra.yaml;
+fi
+
+
+if [ -z "$CLUSTER_NAME" ]; then echo "No cluster name set" ; else 
+	sed -i -e "s/^# cluster_name:.*/cluster_name: $CLUSTER_NAME/" /etc/cassandra/cassandra.yaml
+fi
+
 service cassandra start && sleep 3
 exec "$@"
